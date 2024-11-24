182bda01ef8c444f03722e0389f266dcc8e67caf
KonstantinKok <konstantin.nikolaevish.14.11@gmail.com>
2024-11-24 16:54:53 +0300

    1.0
    1.0

--- business_card/news/views.py
+++ business_card/news/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Article
 from .forms import ArticleForm
+from django.views.generic.edit import DeleteView
 
 
 def news_home(request):
     news = Article.objects.order_by('-date')
     return render(request, 'news/news_home.html', {'news':news})
+
+class NewsDeleteView(DeleteView):
+    model = Article
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
 
 def create(request):
     error = ''
